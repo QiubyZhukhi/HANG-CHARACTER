ea460302e1574d3bd54de990ff3d3ecb5c28dd3d
QiubyZhukhi <qzhukhi@gmail.com>
2018-05-05 07:32:20 -0700

    Update hang.py

--- hang.py
+++ hang.py
@@ -1,12 +1,14 @@
 #coding:utf8
 #qpy:3
 #qpy:console
-
-print("This is console module")
+import sys
 import sl4a
+reload(sys)
+sys.setdefaultencoding('utf-8')
 text = input("Text: ")
 def pesan(t):
     droid = sl4a.Android()
     p = "\u200e\u200f" * 1000
     droid.setClipboard(t.replace(" "," "+p))
-pesan(text)
+if __name__ == "__main__":
+    pesan(text)
